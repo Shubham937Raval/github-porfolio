fa2fec4e451cdba85f9ccc01c77b1527ac046991
Shubham Raval <shubham.raval@evital.in>
2025-01-11 00:03:11 +0530

    revert changes

--- app/api/database/schema.js
+++
@@ -1,9 +0,0 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
-
-export const visitors = sqliteTable("visitors", {
-  id: integer("id").primaryKey().notNull(),
-  ip: text("ip").notNull(),
-  userAgent: text("user_agent").notNull(),
-  timestamp: text("timestamp").notNull(),
-  duration: integer("duration").notNull(),
-});
