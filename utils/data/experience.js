b5a391564dd2be73a3b3ad58bbda781b72c710b2
Shubham Raval <shubham.raval@evital.in>
2025-01-10 21:50:35 +0530

    added experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -4,5 +4,11 @@ export const experiences = [
     title: 'MERN Stack InternShip',
     company: "NCode Technologies",
     duration: "(Nov 2023 - May 2024)"
+  },
+  {
+    id: 2,
+    title: "Backend Engineer",
+    company: "eVitalRx",
+    duration: "(June 2024 - Present)"
   }
 ]
